dfa9a7b16e9c806edc7a720ba474c06531cb27e1
Caroline Yao <Caroline.Yiqi.Yao@gmail.com>
2021-04-10 23:50:19 -0400

    Fix setup script

--- setup.py
+++ setup.py
@@ -8,14 +8,15 @@ os.system("pipenv install")
 os.system("echo python dependencies installation completed")
 
 os.system("echo Setting up flask environment")
-os.system("cd ./src/Services")
+working_directory = os.getcwd()
+os.chdir(working_directory + "/src/Services")
 os.system("export FLASK_APP=endpoints.py")
 os.system("echo Flask environmentn setup completed")
 
-os.system("cd ./reactjs")
+os.chdir(working_directory + "/reactjs")
 os.system("echo Installing react dependencies")
 os.system("npm install")
 
-os.system("cd ..")
+os.chdir(working_directory)
 os.system("echo setup has completed")
 os.system("echo Goodbye")
